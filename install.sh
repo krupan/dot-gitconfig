3f8f7dc63eee4be8560a739093c0b6a0850fcbc2
Bryan Murdock <bmurdock@gmail.com>
2020-11-14 09:33:51 -0700

    intaller: backup any existing .gitconfig first

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@ set -e
 set -x
 REPO_DIR=$(pwd)
 cd ~/
-ln -fs $REPO_DIR/dot-gitconfig .gitconfig
+if [ -f ~/.gitconfig ]; then
+    mv ~/.gitconfig{,.bak}
+fi
+ln -s $REPO_DIR/dot-gitconfig .gitconfig
 
